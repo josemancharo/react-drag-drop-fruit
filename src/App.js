f54963e91fafaa137d94009674fbd2affcf8aa06
josemancharo <63312124+josemancharo@users.noreply.github.com>
2021-05-19 21:05:02 -0500

    made images local

--- src/App.js
+++ src/App.js
@@ -16,11 +16,7 @@ class App extends React.Component {
           style={{ display: this.state.draggableVisibility }}
           dropData={{ type: "Orange" }}
         >
-          <img
-            src="https://media.istockphoto.com/photos/orange-fruit-isolated-on-a-white-background-picture-id494037460?k=6&m=494037460&s=612x612&w=0&h=9CJ0RvlNLk3BMJ4ud2JmyQR7t_fUY7bLw8wl4Tk__kY="
-            width="100px"
-            alt=""
-          />
+          <img src="orange.jpg" width="100px" alt="" />
         </DragDropContainer>
 
         <DragDropContainer
@@ -28,11 +24,7 @@ class App extends React.Component {
           style={{ display: this.state.draggableVisibility }}
           dropData={{ type: "Apple" }}
         >
-          <img
-            src="https://media.istockphoto.com/photos/bite-on-a-red-apple-picture-id475190475?k=6&m=475190475&s=612x612&w=0&h=9yexhsRfQ5-f-CKOMkkZiebbgOJuUq9wsbEfmeUmqGw="
-            width="100px"
-            alt=""
-          />
+          <img src="apple.jpg" width="100px" alt="" />
         </DragDropContainer>
 
         <DropTarget
@@ -42,11 +34,7 @@ class App extends React.Component {
             alert("You put the orange in the box!");
           }}
         >
-          <img
-            src="https://media.istockphoto.com/photos/open-cardboard-box-isolated-on-white-with-clipping-path-picture-id504426400?k=6&m=504426400&s=170667a&w=0&h=k4uKpnGrPRDXVFjiXzMwqukovpixMuVZeArhy9e-GMM="
-            width="250px"
-            alt=""
-          />
+          <img src="box.jpg" width="250px" alt="" />
         </DropTarget>
 
         <DropTarget
@@ -57,11 +45,7 @@ class App extends React.Component {
             window.location.href = "https://youtu.be/dQw4w9WgXcQ";
           }}
         >
-          <img
-            src="https://media.istockphoto.com/photos/open-laundry-basket-picture-id613307728?k=6&m=613307728&s=612x612&w=0&h=gKYImHwIOWTqUTkWlHZoEb4ezbOVnVItF_68mpvM1cE="
-            width="200px"
-            alt=""
-          />
+          <img src="basket.jpg" width="200px" alt="" />
         </DropTarget>
       </>
     );
